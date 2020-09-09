2101c958fc1d0d4e85f133d11ec68f0c6bb39c36
Marc König <mkoenig.mail@icloud.com>
2020-09-09 09:39:28 +0200

    Added persistent _timestamp

--- src/dom/common.ts
+++ src/dom/common.ts
@@ -1,3 +1,7 @@
+// Time interval that a edit is stored if not loaded in between
+const _storageTimeToLife = 60 * 60 * 1000;
+
+// Check if sessionStorage is avilable
 function storageAvaiable() {
 	if (typeof sessionStorage !== 'undefined') {
 		try {
@@ -13,18 +17,30 @@ function storageAvaiable() {
 
 function setData(key: string, value: string): void {
 	if (storageAvaiable()) {
+		sessionStorage.setItem(key + '_timestamp', Date.now().toString());
 		sessionStorage.setItem(key, value);
 	} else {
 		var d = new Date();
-		// Store 1h
-		d.setTime(d.getTime() + 1 * 60 * 60 * 1000);
+		d.setTime(d.getTime() + _storageTimeToLife);
 		var expires = 'expires=' + d.toUTCString();
 		document.cookie = key + '=' + value + ';' + expires + ';path=/';
 	}
 }
+
 function getData(key: string): string {
 	if (storageAvaiable()) {
-		return sessionStorage.getItem(key);
+		let d = parseInt(sessionStorage.getItem(key + '_timestamp'));
+
+		if (Date.now() - d > _storageTimeToLife) {
+			sessionStorage.removeItem(key);
+			sessionStorage.removeItem(key + '_timestamp');
+			return '';
+		} else {
+			// Update timestamp
+			sessionStorage.setItem(key + '_timestamp', Date.now().toString());
+		}
+
+		return sessionStorage.getItem(key) || '';
 	} else {
 		var name = key + '=';
 		var decodedCookie = decodeURIComponent(document.cookie);
@@ -43,7 +59,15 @@ function getData(key: string): string {
 }
 
 export const PersistentStorage = {
+	/**
+	 * Stores a string in a local persistent storage container.
+	 * Expires after 1h
+	 */
 	setData: setData,
+	/**
+	 * Loads a string from a local persisten storage container
+	 * Returns "" if not existent
+	 */
 	getData: getData
 };
 
