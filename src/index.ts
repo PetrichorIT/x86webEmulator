f05d5c1df6075a643d8d0d4faad6eec0222ffddd
Marc König <mkoenig.mail@icloud.com>
2020-09-12 11:07:44 +0200

    Added lib.test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { App } from './App';
 import * as x86 from './x86';
 import { DOMApp } from './dom/DOMApp';
-import fib from './x86/lib/fib';
-import { string, stringEntryPoints } from './x86/lib/string';
+import { includeLibs } from './x86/lib/lib';
 
 let app = new App(x86);
-
-app.loadLib('fib', fib, [ 'fib', 'clrMem' ]);
-app.loadLib('string', string, stringEntryPoints);
+includeLibs(app);
 
 let domApp = new DOMApp(app);
